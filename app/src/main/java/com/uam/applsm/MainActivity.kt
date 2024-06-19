dc13c82740e129ef44479a3d4e1022006085cac9
rosaainz <al2173040136@azc.uam.mx>
2024-06-19 17:17:25 -0600

    add video back camera

--- app/src/main/java/com/uam/applsm/MainActivity.kt
+++ app/src/main/java/com/uam/applsm/MainActivity.kt
@@ -1,14 +1,25 @@
 package com.uam.applsm
 
 import android.os.Bundle
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.activity.ComponentActivity
 import android.widget.Button
 import android.widget.Toast
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.core.app.ActivityCompat
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
 
 class MainActivity : ComponentActivity() {
     //Elementos de la interfaz de usuario
     private lateinit var btnCapture: Button
     private lateinit var btnEndCapture: Button
+    private lateinit var previewView: PreviewView
+    private lateinit var textView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,6 +27,8 @@ class MainActivity : ComponentActivity() {
 
         btnCapture = findViewById(R.id.btnCapture)
         btnEndCapture = findViewById(R.id.btnEndCapture)
+        previewView = findViewById(R.id.previewView)
+        textView = findViewById(R.id.textView)
 
         btnCapture.setOnClickListener {
             Toast.makeText(this, "Capturar fotogramas", Toast.LENGTH_SHORT).show()
@@ -24,5 +37,31 @@ class MainActivity : ComponentActivity() {
         btnEndCapture.setOnClickListener {
             Toast.makeText(this, "Finalizar captura", Toast.LENGTH_SHORT).show()
         }
+
+        // Configurar permisos para la cámara
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), 50)
+        } else {
+            startCamera()
+        }
+    }
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+            val cameraProvider = cameraProviderFuture.get()
+            val preview = Preview.Builder().build().also {
+                it.setSurfaceProvider(previewView.surfaceProvider)
+            }
+
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }, ContextCompat.getMainExecutor(this))
     }
 }
