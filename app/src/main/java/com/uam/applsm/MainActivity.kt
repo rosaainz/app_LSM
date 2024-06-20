5e92a0b445c7bc3c15fb5a41f8e73e40aca45c0d
rosaainz <al2173040136@azc.uam.mx>
2024-06-19 18:18:46 -0600

    change for front camera

--- app/src/main/java/com/uam/applsm/MainActivity.kt
+++ app/src/main/java/com/uam/applsm/MainActivity.kt
@@ -17,25 +17,21 @@ import androidx.camera.view.PreviewView
 class MainActivity : ComponentActivity() {
     //Elementos de la interfaz de usuario
     private lateinit var btnCapture: Button
-    private lateinit var btnEndCapture: Button
     private lateinit var previewView: PreviewView
     private lateinit var textView: TextView
+    private var cameraSelector: CameraSelector = CameraSelector.DEFAULT_FRONT_CAMERA
+    private var cameraProvider: ProcessCameraProvider? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         btnCapture = findViewById(R.id.btnCapture)
-        btnEndCapture = findViewById(R.id.btnEndCapture)
         previewView = findViewById(R.id.previewView)
         textView = findViewById(R.id.textView)
 
         btnCapture.setOnClickListener {
             Toast.makeText(this, "Capturar fotogramas", Toast.LENGTH_SHORT).show()
-        }
-
-        btnEndCapture.setOnClickListener {
-            Toast.makeText(this, "Finalizar captura", Toast.LENGTH_SHORT).show()
         }
 
         // Configurar permisos para la cámara
@@ -49,19 +45,20 @@ class MainActivity : ComponentActivity() {
         val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
 
         cameraProviderFuture.addListener({
-            val cameraProvider = cameraProviderFuture.get()
-            val preview = Preview.Builder().build().also {
-                it.setSurfaceProvider(previewView.surfaceProvider)
-            }
-
-            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
-
-            try {
-                cameraProvider.unbindAll()
-                cameraProvider.bindToLifecycle(this, cameraSelector, preview)
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
+            cameraProvider = cameraProviderFuture.get()
+            bindCameraUseCases()
         }, ContextCompat.getMainExecutor(this))
+    }
+    private fun bindCameraUseCases() {
+        val preview = Preview.Builder().build().also {
+            it.setSurfaceProvider(previewView.surfaceProvider)
+        }
+
+        try {
+            cameraProvider?.unbindAll()
+            cameraProvider?.bindToLifecycle(this, cameraSelector, preview)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 }
