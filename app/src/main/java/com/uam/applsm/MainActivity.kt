bae8a99ba2db95aaf09f9e67953a20335cd23992
rosaainz <al2173040136@azc.uam.mx>
2024-06-23 15:20:52 -0600

    sendImage byteArray, chage for form-data

--- app/src/main/java/com/uam/applsm/MainActivity.kt
+++ app/src/main/java/com/uam/applsm/MainActivity.kt
@@ -1,26 +1,32 @@
 package com.uam.applsm
 
+
 import android.Manifest
+import android.content.ContentValues.TAG
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.os.Environment
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.camera.core.CameraSelector
-import androidx.camera.core.Preview
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.ImageProxy
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import okhttp3.MediaType
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.RequestBody
-import okhttp3.Response
+import okhttp3.*
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
 import java.io.IOException
-import android.os.StrictMode
 import android.util.Log
+import androidx.camera.core.Preview
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.RequestBody.Companion.toRequestBody
 
@@ -31,8 +37,9 @@ class MainActivity : ComponentActivity() {
     private lateinit var textView: TextView
     private var cameraSelector: CameraSelector = CameraSelector.DEFAULT_FRONT_CAMERA
     private var cameraProvider: ProcessCameraProvider? = null
-    private val URL = "http://192.168.0.74:4000"
-    private val JSON: MediaType = "application/json; charset=utf-8".toMediaType()
+    private var imageCapture: ImageCapture? = null
+    private val URL = "http://192.168.0.74:4000/predict"
+    private val MEDIA_TYPE_JPEG = "image/jpeg".toMediaType()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,7 +52,9 @@ class MainActivity : ComponentActivity() {
         textView.text = "frase"
 
         btnCapture.setOnClickListener {
-            Toast.makeText(this, "Capturar fotogramas", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Capturar lsm", Toast.LENGTH_SHORT).show()
+            Log.d("MainActivity", "Botón 'Iniciar Captura' clickeado")
+            capturePhoto()
         }
 
         // Permisos para la cámara
@@ -60,14 +69,14 @@ class MainActivity : ComponentActivity() {
         }
 
         // Permitir operaciones de red en el hilo principal
-        val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
-        StrictMode.setThreadPolicy(policy)
-
-        // datos que se van a enviar
-        val myData = """{"data":"eeeeee"}"""
-        sendPostRequest(myData)
+        //val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+        //StrictMode.setThreadPolicy(policy)
     }
 
+    override fun onStop() {
+        super.onStop()
+        cameraProvider?.unbindAll()
+    }
     private fun startCamera() {
         val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
 
@@ -78,37 +87,115 @@ class MainActivity : ComponentActivity() {
     }
 
     private fun bindCameraUseCases() {
-        val preview = Preview.Builder().build().also {
-            it.setSurfaceProvider(previewView.surfaceProvider)
-        }
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
 
-        try {
-            cameraProvider?.unbindAll()
-            cameraProvider?.bindToLifecycle(this, cameraSelector, preview)
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
+        cameraProviderFuture.addListener({
+            cameraProvider = cameraProviderFuture.get()
+
+            // Configurar preview
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(previewView.surfaceProvider)
+                }
+
+            // Configurar captura de imagen
+            imageCapture = ImageCapture.Builder()
+                .build()
+
+            try {
+                // Desvincular todas las cámaras previas antes de volver a unir
+                cameraProvider?.unbindAll()
+
+                // Vincular cámaras al ciclo de vida de la actividad
+                cameraProvider?.bindToLifecycle(this, cameraSelector, preview, imageCapture)
+
+            } catch (e: Exception) {
+                Log.e(TAG, "Error al configurar la cámara: ${e.message}")
+                e.printStackTrace()
+            }
+        }, ContextCompat.getMainExecutor(this))
     }
 
-    private fun sendPostRequest(data: String) {
+
+    private fun capturePhoto() {
+        val imageCapture = imageCapture ?: return
+
+        imageCapture.takePicture(ContextCompat.getMainExecutor(this), object : ImageCapture.OnImageCapturedCallback() {
+            override fun onCaptureSuccess(image: ImageProxy) {
+                val bitmap = imageToBitmap(image)
+                saveImage(bitmap)
+                sendImage(bitmap)
+                image.close()
+            }
+
+            override fun onError(exception: ImageCaptureException) {
+                exception.printStackTrace()
+            }
+        })
+    }
+    private fun imageToBitmap(image: ImageProxy): Bitmap {
+        val buffer = image.planes[0].buffer
+        val bytes = ByteArray(buffer.capacity())
+        buffer.get(bytes)
+        return BitmapFactory.decodeByteArray(bytes, 0, bytes.size, null)
+    }
+
+    private fun saveImage(bitmap: Bitmap) {
+        // Crear carpeta si no existe
+        val folder = File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), "CapturedImages")
+        if (!folder.exists()) {
+            folder.mkdirs()
+        }
+
+        // Guardar imagen en la carpeta
+        val fileName = "IMG_${System.currentTimeMillis()}.jpg"
+        val file = File(folder, fileName)
+        try {
+            val outputStream = FileOutputStream(file)
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+            outputStream.flush()
+            outputStream.close()
+            Log.d("MainActivity", "Imagen guardada: ${file.absolutePath}")
+        } catch (e: IOException) {
+            e.printStackTrace()
+            Log.e("MainActivity", "Error al guardar la imagen: ${e.message}")
+        }
+    }
+    private fun sendImage(bitmap: Bitmap) {
         val client = OkHttpClient()
 
-        // Crear cuerpo de la solicitud
-        val body = data.toRequestBody(JSON)
+        // Convertir el bitmap a un array de bytes en formato JPEG
+        val outputStream = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+        val requestBody = outputStream.toByteArray().toRequestBody(MEDIA_TYPE_JPEG)
 
-        // Crear solicitud POST
+
+        // Crear solicitud POST para enviar la imagen
         val request = Request.Builder()
             .url(URL)
-            .post(body)
-            .header("Content-Type", "application/json")
+            .post(requestBody)
+            .header("Content-Type", "image/jpeg")
             .build()
 
-        // Enviar solicitud
+        //Log.d("HTTP_POST", "Tamaño byte array: ${byteArray.size}")
+
         try {
-            client.newCall(request).execute().use { response ->
-                if (!response.isSuccessful) throw IOException("Unexpected code $response")
-                Log.d("HTTP_POST", "Response: ${response.body?.string()}")
-            }
+            client.newCall(request).enqueue(object : Callback {
+                override fun onResponse(call: Call, response: Response) {
+                    if (response.isSuccessful) {
+                        val responseBody = response.body?.string()
+                        Log.d("HTTP_POST", "Response: $responseBody")
+                    } else {
+                        Log.e("HTTP_POST", "Error en el servidor: ${response.code}")
+                    }
+                }
+
+                override fun onFailure(call: Call, e: IOException) {
+                    e.printStackTrace()
+                    Log.e("HTTP_POST", "Error al hacer la solicitud al servidor: ${e.message}")
+                }
+            })
         } catch (e: IOException) {
             e.printStackTrace()
             Log.e("HTTP_POST", "Error: ${e.message}")
