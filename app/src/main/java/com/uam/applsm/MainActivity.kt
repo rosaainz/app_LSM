96c8dce987ec941450f79d9ecd5667733d1bf71b
rosaainz <al2173040136@azc.uam.mx>
2024-06-24 19:03:56 -0600

    send photo

--- app/src/main/java/com/uam/applsm/MainActivity.kt
+++ app/src/main/java/com/uam/applsm/MainActivity.kt
@@ -6,6 +6,9 @@ import android.content.ContentValues.TAG
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.ImageFormat
+import android.graphics.YuvImage
+import android.media.Image
 import android.os.Bundle
 import android.os.Environment
 import android.widget.Button
@@ -26,9 +29,11 @@ import java.io.File
 import java.io.FileOutputStream
 import java.io.IOException
 import android.util.Log
+import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.Preview
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.RequestBody.Companion.toRequestBody
+import java.util.concurrent.Executors
 
 
 class MainActivity : ComponentActivity() {
@@ -38,10 +43,12 @@ class MainActivity : ComponentActivity() {
     private lateinit var textView: TextView
     private var cameraSelector: CameraSelector = CameraSelector.DEFAULT_FRONT_CAMERA
     private var cameraProvider: ProcessCameraProvider? = null
+    private var imageAnalysis: ImageAnalysis? = null
     private var imageCapture: ImageCapture? = null
     private val URL = "http://192.168.0.74:4000/predict"
     private val MEDIA_TYPE_JPEG = "image/jpeg".toMediaType()
     private val executor = Executors.newSingleThreadExecutor()
+    private var capturing = false
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -56,14 +63,16 @@ class MainActivity : ComponentActivity() {
         textView.text = "frase"
 
         btnCapture.setOnClickListener {
-            Toast.makeText(this, "Capturar lsm", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "Capturar fotogramas", Toast.LENGTH_SHORT).show()
             Log.d("MainActivity", "Botón 'Iniciar Captura' clickeado")
             capturePhoto()
+            //startCapture()
         }
 
         btnEndCapture.setOnClickListener{
             Toast.makeText(this,"Finalizar captura", Toast.LENGTH_SHORT).show()
             Log.d("MainActivity", "Botón 'Finalizar Captura' clickeado")
+            stopCapture()
         }
 
         // Permisos para la cámara
@@ -108,15 +117,30 @@ class MainActivity : ComponentActivity() {
                     it.setSurfaceProvider(previewView.surfaceProvider)
                 }
 
-            // Configurar captura de imagen
+            // Captura de imagen
             imageCapture = ImageCapture.Builder()
                 .build()
+
+            //Captura de fotograma
+            imageAnalysis = ImageAnalysis.Builder()
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .build()
+                .also {
+                    it.setAnalyzer(executor) { image ->
+                        if (capturing) {
+                            processImage(image)
+                        }
+                        image.close()
+                    }
+                }
+
 
             try {
                 // Desvincular todas las cámaras previas antes de volver a unir
                 cameraProvider?.unbindAll()
 
                 // Vincular cámaras al ciclo de vida de la actividad
+               // cameraProvider?.bindToLifecycle(this, cameraSelector, preview,imageAnalysis)
                 cameraProvider?.bindToLifecycle(this, cameraSelector, preview, imageCapture)
 
             } catch (e: Exception) {
@@ -125,52 +149,61 @@ class MainActivity : ComponentActivity() {
             }
         }, ContextCompat.getMainExecutor(this))
     }
-
-
-    private fun capturePhoto() {
-        val imageCapture = imageCapture ?: return
-
-        imageCapture.takePicture(ContextCompat.getMainExecutor(this), object : ImageCapture.OnImageCapturedCallback() {
-            override fun onCaptureSuccess(image: ImageProxy) {
-                val bitmap = imageToBitmap(image)
-               // saveImage(bitmap)
-                sendImage(bitmap)
-                image.close()
-            }
-
-            override fun onError(exception: ImageCaptureException) {
-                exception.printStackTrace()
-            }
-        })
+    private fun startCapture(){
+        capturing = true
     }
-    private fun imageToBitmap(image: ImageProxy): Bitmap {
+    private fun stopCapture(){
+        capturing = false
+    }
+    private fun processImage(image: ImageProxy) {
+        val bitmap = imageToBitmap(image)
+        if (bitmap != null) {
+            sendImage(bitmap)
+        } else {
+            Log.e(TAG, "Error al convertir la imagen a bitmap")
+        }
+        image.close()
+    }
+    private fun imageToBitmap(image: ImageProxy): Bitmap { //?
         val buffer = image.planes[0].buffer
         val bytes = ByteArray(buffer.capacity())
         buffer.get(bytes)
         return BitmapFactory.decodeByteArray(bytes, 0, bytes.size, null)
+//        return try {
+//            // Solo procesar imágenes en formato YUV_420_888
+//            if (image.format != ImageFormat.YUV_420_888) {
+//                Log.e(TAG, "Formato de imagen no compatible: ${image.format}")
+//                return null
+//            }
+//
+//            val yBuffer = image.planes[0].buffer // Y
+//            val uBuffer = image.planes[1].buffer // U
+//            val vBuffer = image.planes[2].buffer // V
+//
+//            val ySize = yBuffer.remaining()
+//            val uSize = uBuffer.remaining()
+//            val vSize = vBuffer.remaining()
+//
+//            val nv21 = ByteArray(ySize + uSize + vSize)
+//
+//            // Convertir YUV_420_888 a NV21
+//            yBuffer.get(nv21, 0, ySize)
+//            vBuffer.get(nv21, ySize, vSize)
+//            uBuffer.get(nv21, ySize + vSize, uSize)
+//
+//            val yuvImage = YuvImage(nv21, ImageFormat.NV21, image.width, image.height, null)
+//            val out = ByteArrayOutputStream()
+//            yuvImage.compressToJpeg(android.graphics.Rect(0, 0, image.width, image.height), 100, out)
+//            val imageBytes = out.toByteArray()
+//
+//            BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.size)
+//        } catch (e: Exception) {
+//            Log.e(TAG, "Error al convertir la imagen: ${e.message}")
+//            e.printStackTrace()
+//            null
+//        }
     }
 
-    private fun saveImage(bitmap: Bitmap) {
-        // Crear carpeta si no existe
-        val folder = File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), "CapturedImages")
-        if (!folder.exists()) {
-            folder.mkdirs()
-        }
-
-        // Guardar imagen en la carpeta
-        val fileName = "IMG_${System.currentTimeMillis()}.jpg"
-        val file = File(folder, fileName)
-        try {
-            val outputStream = FileOutputStream(file)
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
-            outputStream.flush()
-            outputStream.close()
-            Log.d("MainActivity", "Imagen guardada: ${file.absolutePath}")
-        } catch (e: IOException) {
-            e.printStackTrace()
-            Log.e("MainActivity", "Error al guardar la imagen: ${e.message}")
-        }
-    }
     private fun sendImage(bitmap: Bitmap) {
         val client = OkHttpClient()
 
@@ -178,6 +211,12 @@ class MainActivity : ComponentActivity() {
         val outputStream = ByteArrayOutputStream()
         bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
         val byteArray = outputStream.toByteArray()
+
+        //verificar que el bitmap  no este vacio
+        if (byteArray.isEmpty()) {
+            Log.e(TAG, "Error: byteArray está vacío")
+            return
+        }
 
         // Rquest body de la imagen
         val requestFile = byteArray.toRequestBody(MEDIA_TYPE_JPEG)
@@ -215,6 +254,42 @@ class MainActivity : ComponentActivity() {
         } catch (e: IOException) {
             e.printStackTrace()
             Log.e("HTTP_POST", "Error: ${e.message}")
+        }
+    }
+    private fun capturePhoto() {
+        val imageCapture = imageCapture ?: return
+
+        imageCapture.takePicture(ContextCompat.getMainExecutor(this), object : ImageCapture.OnImageCapturedCallback() {
+            override fun onCaptureSuccess(image: ImageProxy) {
+                val bitmap = imageToBitmap(image)
+               // saveImage(bitmap)
+                sendImage(bitmap)
+                image.close()
+            }
+            override fun onError(exception: ImageCaptureException) {
+                exception.printStackTrace()
+            }
+        })
+    }
+    private fun saveImage(bitmap: Bitmap) {
+        // Crear carpeta si no existe
+        val folder = File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), "CapturedImages")
+        if (!folder.exists()) {
+            folder.mkdirs()
+        }
+
+        // Guardar imagen en la carpeta
+        val fileName = "IMG_${System.currentTimeMillis()}.jpg"
+        val file = File(folder, fileName)
+        try {
+            val outputStream = FileOutputStream(file)
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+            outputStream.flush()
+            outputStream.close()
+            Log.d("MainActivity", "Imagen guardada: ${file.absolutePath}")
+        } catch (e: IOException) {
+            e.printStackTrace()
+            Log.e("MainActivity", "Error al guardar la imagen: ${e.message}")
         }
     }
 }
